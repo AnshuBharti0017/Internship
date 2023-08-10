cfee7e3eaadd39732809fbb21f4d382964f4b9ff
AnshuBharti0017 <141671623+AnshuBharti0017@users.noreply.github.com>
2023-08-10 17:47:20 +0530

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+using namespace std;
+void main()
+{
+	int a;
+	cout<<"Enter any number :- ";
+	cin>>a;
+
+	if(a%2==0)
+	{
+		cout<<a<<" is even\n";
+	}
+	else
+	{
+		cout<<a<<" is odd\n";
+	}
+
+}
